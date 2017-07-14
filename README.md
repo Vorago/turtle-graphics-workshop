84e115dc26d9d577954b8edd0c409c48d359b0d0
Andrew Naydyonock <a.vorago@gmail.com>
2017-07-14 22:46:59 +0300

    Examples added

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-## Getting started
+# Getting started
+
+## Building on macOS and Linux
 
 ```
+cd turtle-graphics-workshop/
 ./gradlew clean build idea
+```
+
+## Building on Windows
+
+```
+cd turtle-graphics-workshop/
+gradlew clean build idea
 ```
--- src/main/java/lv/rtg/Main.java
+++ src/main/java/lv/rtg/Main.java
@@ -1,8 +1,45 @@
 package lv.rtg;
 
+import cheloniidae.RealtimeTurtle;
+
 public class Main {
+
+    private static RealtimeTurtle turtle = new RealtimeTurtle();
+
     public static void main(String[] args) {
-        Square square = new Square();
-        square.draw();
+        turtle.color(0, 0, 0);
+        turtle.size(2);
+
+        turtle.forward(100);
+        turtle.right(90);
+        turtle.forward(100);
+    }
+
+    private static void cycleWhile() {
+        int side = 0;
+        while (side < 4) {
+            turtle.forward(100);
+            turtle.right(90);
+            side = side + 1;
+        }
+    }
+
+    private static void cycleFor() {
+        for (int i = 0; i < 4; i++) {
+            turtle.forward(100);
+            turtle.right(90);
+        }
+    }
+
+    private static void cycleWithCondition() {
+        turtle.color(0, 255, 0);
+        for (int i = 0; i < 4; i++) {
+            if (i == 2) {
+                turtle.color(0, 0, 255);
+            }
+
+            turtle.forward(100);
+            turtle.right(90);
+        }
     }
 }
